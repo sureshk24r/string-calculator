2ea87b2dbc3d9d5c1f5e9c3777d4ac97b898a9d5
sureshk24r <107083914+sureshk24r@users.noreply.github.com>
2022-06-07 19:27:30 -0700

    Requirement 1
    Support a maximum of 2 numbers using a comma delimiter. Throw an exception when more than 2 numbers are provided
    examples: 20 will return 20; 1,5000 will return 5001; 4,-3 will return 1
    empty input or missing numbers should be converted to 0
    invalid numbers should be converted to 0 e.g. 5,tytyt will return 5

---
+++ ChallengeCalculator.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace challenge_calculator
+{
+    public class StringCalculator
+    {
+        public int Add(string userInput)
+        {
+            int calculatedValue = 0;
+            List<int> intList = new List<int>();
+
+            //Split the string based on the separators
+            string[] commaSeparatedNumbers = userInput.Split(new string[] { "," }, StringSplitOptions.None);
+
+            if (commaSeparatedNumbers.Count() > 2)
+                throw new ArgumentException("More than two numbers are not allowed");
+
+            foreach (var number in commaSeparatedNumbers)
+            {
+                int parsedInteger = 0;
+
+                //Try parsing the string to an integer.
+                if (!int.TryParse(number, out parsedInteger))
+                {
+                    //Convert the unparsed string to 0
+                    parsedInteger = 0;
+                }
+
+                intList.Add(parsedInteger);
+                calculatedValue += parsedInteger;
+            }
+
+            Console.WriteLine("Calculated Numbers: " + string.Join(" + ", intList.ToArray()) + " = " + calculatedValue);
+
+            //Return the calculated value
+            return calculatedValue;
+        }
+    }
+}
